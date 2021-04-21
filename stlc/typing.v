eb1b2af39b317ea6593d07babe3701c2634f5046
Paul He <paulhe@cis.upenn.edu>
2021-04-21 13:48:28 -0400

    Add typing rules for recursive types

--- stlc/typing.v
+++ stlc/typing.v
@@ -4,7 +4,14 @@ Inductive type :=
   | TUnit : type
   | TProd : type → type → type
   | TSum : type → type → type
-  | TArrow : type → type → type.
+  | TArrow : type → type → type
+  | TRec (τ : {bind 1 of type})
+  | TVar (x : var).
+
+Instance Ids_type : Ids type. derive. Defined.
+Instance Rename_type : Rename type. derive. Defined.
+Instance Subst_type : Subst type. derive. Defined.
+Instance SubstLemmas_typer : SubstLemmas type. derive. Qed.
 
 Reserved Notation "Γ ⊢ₜ e : τ" (at level 74, e, τ at next level).
 
@@ -22,15 +29,20 @@ Inductive typed (Γ : list type) : expr → type → Prop :=
      Γ ⊢ₜ Case e0 e1 e2 : ρ
   | Lam_typed e τ1 τ2 : τ1 :: Γ ⊢ₜ e : τ2 → Γ ⊢ₜ Lam e : TArrow τ1 τ2
   | App_typed e1 e2 τ1 τ2 :
-     Γ ⊢ₜ e1 : TArrow τ1 τ2 → Γ ⊢ₜ e2 : τ1 → Γ ⊢ₜ App e1 e2 : τ2
+      Γ ⊢ₜ e1 : TArrow τ1 τ2 → Γ ⊢ₜ e2 : τ1 → Γ ⊢ₜ App e1 e2 : τ2
+  | Rec_typed e τ1 τ2 :
+      TArrow τ1 τ2 :: τ1 :: Γ ⊢ₜ e : τ2 → Γ ⊢ₜ Rec e : TArrow τ1 τ2
+  | TFold e τ : Γ ⊢ₜ e : τ.[TRec τ/] → Γ ⊢ₜ Fold e : TRec τ
+  | TUnfold e τ : Γ ⊢ₜ e : TRec τ → Γ ⊢ₜ Unfold e : τ.[TRec τ/]
 where "Γ ⊢ₜ e : τ" := (typed Γ e τ).
 
 Lemma typed_subst_invariant Γ e τ s1 s2 :
   Γ ⊢ₜ e : τ → (∀ x, x < length Γ → s1 x = s2 x) → e.[s1] = e.[s2].
 Proof.
   intros Htyped; revert s1 s2.
-  assert (∀ s1 s2 x, (x ≠ 0 → s1 (pred x) = s2 (pred x)) → up s1 x = up s2 x).
-  { rewrite /up=> s1 s2 [|x] //=; auto with f_equal lia. }
+  assert (∀ {A} `{Ids A} `{Rename A} (s1 s2 : nat → A) x,
+    (x ≠ 0 → s1 (pred x) = s2 (pred x)) → up s1 x = up s2 x).
+  { intros A H1 H2. rewrite /up=> s1 s2 [|x] //=; auto with f_equal lia. }
   induction Htyped => s1 s2 Hs; f_equal/=; eauto using lookup_lt_Some with lia.
 Qed.
 
