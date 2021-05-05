6d16ed23d6b58cec7a24716b3988554f4f277161
euisuny <ey222@cornell.edu>
2021-05-05 19:22:54 -0400

    WIP re-defining val using separate construct from CBPV values

--- src/lang1.v
+++ src/lang1.v
@@ -1,10 +1,26 @@
 From iris.program_logic Require Export language ectx_language ectxi_language.
 From cbpv Require Import base syntax semantics.
-Definition of_val (n : nat) (v : value n) : comp n := ret v.
 
-Definition to_val (n : nat) (c : comp n) : option (value n) :=
+Inductive val :=
+  | PairV (v1 v2 : val)
+  | UnitV
+  | InjLV (v : val)
+  | InjRV (v : val).
+
+Fixpoint of_val {n : nat} (v : val) : comp n :=
+  match v with
+  | PairV v1 v2 => tuple (of_val v1) (of_val v2)
+  | UnitV => cu
+  | InjLV v1 => proj true (of_val v1)
+  | InjRV v1 => proj false (of_val v1)
+  end.
+
+Fixpoint to_val {n : nat} (c : comp n) : option val :=
   match c with
-  | ret v => Some v
+  | cu => Some UnitV
+  | tuple e1 e2 => v1 ← to_val e1 ; v2 ← to_val e2 ; Some (PairV v1 v2)
+  | proj true e1 => v1 ← to_val e1 ; Some (InjLV v1)
+  | proj false e1 => v1 ← to_val e1 ; Some (InjRV v1)
   | _ => None
   end.
 
@@ -12,7 +28,7 @@ Variant head_step {n : nat} : comp n -> unit -> list unit -> comp n -> unit -> l
 | strong_head_step c1 c2 : strong_step c1 c2 ->
                      head_step c1 tt nil c2 tt nil.
 
-Lemma Inj_fill_vctx n m:
+Lemma fill_item_inj n m:
   ∀ Ki : cctx false n m, Inj eq eq (@fillc n _ _ Ki).
 Proof.
   revert n m.
@@ -23,53 +39,67 @@ Proof.
   end.
 Qed.
 
-Lemma to_of_val n v : to_val n (of_val n v) = Some v.
-Proof. by induction v; try simplify_option_eq. Qed.
+(** Basic properties about the language *)
+Lemma to_of_val n v : @to_val n (@of_val n v) = Some v.
+Proof. induction v; try simplify_option_eq. all : try eauto. Qed.
 
-Require Import Coq.Program.Equality.
-Lemma lang_mixin n : EctxiLanguageMixin (of_val n) (to_val n) (fillc (t := false)) head_step.
+Lemma of_to_val:
+  ∀ n (e : comp n) (v : val), @to_val n e = Some v → @of_val n v = e.
 Proof.
-  split; auto.
-  - unfold of_val, to_val. intros. destruct e; inversion H; auto.
-  - intros. inv H. inv H0.
-    (* revert n m c1 K c2 H. eapply mutind_vctx_cctx; cbn; intuition. *)
-    (* all : repeat intro; intuiition; *)
-    (* destruct K; eauto. clear H. destruct y. *)
-    (* cbn.  *)
-  (* destruct K; simpl; eauto. inv y. eauto. inv H; destruct K; simpl; auto.  exintuition. *)
-    admit.
-  - revert n.
-    (* eapply mutind_vctx_cctx; cbn; exintuition. *)
-    (* Unshelve. 5 : exact false. *)
-    admit.
-    (* eauto.  *)
-
-    (* dependent induction Ki. *)
-    (* 1-3: intros; eauto.  cbn in H; try solve [inv H; inv H0]. *)
-    (* revert e H. *)
-    (* dependent induction v. inv y. *)
-    (* all : intros; try eapply IHv; eauto. *)
-
-    (* eapply  *)
-
-  (* inv H. inv H0.  *)
-
-
-
-  - apply Inj_fill.
-  - induction Ki1, Ki2; intros; try discriminate; simplify_eq;
-    repeat match goal with
-           | H : to_val (of_val _) = None |- _ => by rewrite to_of_val in H
-           end; auto.
-
-    eapply IHKi1 in H1; subst; eauto.
-    eapply IHKi1 in H2; subst; eauto.
-    eapply IHKi1 in H1; subst; eauto.
-  - destruct Ki; inversion_clear 1; simplify_option_eq; eauto.
-    inv H0. 
-    inv H0. 
-  split; apply _ || eauto using to_of_val, of_to_val, val_head_stuck,
-         fill_item_.,`val, fill_item_no_val_inj, head_ctx_step_val.
+  intros n e; induction e; try destruct b; intros; simplify_option_eq; auto with f_equal.
 Qed.
 
-EctxiLanguageMixin
+Instance of_val_inj n : Inj (=) (=) (@of_val n).
+Proof.
+  intros ?? Hv; apply (stdpp.base.inj Some); rewrite -!(to_of_val n) Hv. auto. Qed.
+
+Require Import Coq.Program.Equality.
+Lemma fill_item_val n:
+ ∀ (Ki : cctx false n n) (e : comp n), is_Some (@to_val n (fillc Ki e)) → is_Some (@to_val n e).
+Proof.
+  intros Ki e [v ?]. dependent induction Ki; simplify_option_eq; eauto.
+  destruct b; simplify_option_eq; eapply IHKi; eauto.
+Qed.
+
+Lemma head_ctx_step_val n
+      (Ki : cctx false n n) (e : comp n) (σ1 : ()) (κ : list ()) (e2 : comp n) (σ2 : ()) (efs : list (comp n)):
+    head_step (fillc Ki e) σ1 κ e2 σ2 efs → is_Some (to_val e).
+Proof.
+  dependent induction Ki.
+  (* cctxHole *)
+  - inversion_clear 1; simplify_option_eq; eauto.
+    inv H0.
+Admitted.
+
+(* Qed. *)
+(*   destruct Ki; inversion_clear 1; simplify_option_eq; eauto. *)
+(*   inv y. *)
+(* Admitted. *)
+
+(* Lemma fill_item_no_val_inj n: *)
+(*  ∀ (Ki1 Ki2 : cctx false n n) (e1 e2 : comp n), *)
+(*    @to_val n e1 = None → @to_val n e2 = None → fillc Ki1 e1 = fillc Ki2 e2 → Ki1 = Ki2. *)
+(* Proof. *)
+(*   (* intros Ki1 Ki2;  dependent destruction Ki1; dependent destruction Ki2; intros; try discriminate; simplify_eq; *) *)
+(*   (* repeat match goal with *) *)
+(*   (*         | H : to_val (of_val _) = None |- _ => by rewrite to_of_val in H *) *)
+(*   (*        end; auto. *) *)
+(*   (* cbn. subst. eauto. cbn. unfold cctxHole. red. apply fill_item_inj in H1. inv H1. *) *)
+(*   (* Qed. *) *)
+(* Admitted. *)
+
+Lemma val_head_stuck n e1 σ1 κ e2 σ2 efs : head_step e1 σ1 κ e2 σ2 efs → @to_val n e1 = None.
+Proof. Admitted.
+
+Lemma lang_mixin n : EctxiLanguageMixin (@of_val n) (@to_val n) (fillc (t := false)) head_step.
+Proof.
+  split; auto.
+  - eapply (to_of_val n).
+  - eapply (of_to_val n).
+  - apply val_head_stuck.
+  - apply fill_item_val.
+  - eapply fill_item_inj.
+  - admit.
+    (* apply fill_item_no_val_inj. *)
+  - apply head_ctx_step_val.
+Admitted.
